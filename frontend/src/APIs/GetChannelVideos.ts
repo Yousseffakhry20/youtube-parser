1ffa72ac4e533c329e6435c79aef990164024180
Yousseffakhry20 <yousseffakhry96@gmail.com>
2025-11-28 06:39:05 +0200

    ***Frontend: -Modified BaseURL for APIs.

--- frontend/src/APIs/GetChannelVideos.ts
+++ frontend/src/APIs/GetChannelVideos.ts
@@ -3,12 +3,14 @@ import type { CategoryWithVideos } from "../Types/Types";
 
 const BaseURL = process.env.REACT_APP_API_URL;
 
+console.log(BaseURL);
+
 export const GetChannelVideos = async (validUrls: string[]) => {
   const response = await axios.get<{
     categories: CategoryWithVideos[];
     channelIdentifiers: string[];
   }>(
-    `${BaseURL}/api/channels/videos?channelUrls=${encodeURIComponent(
+    `${BaseURL}api/channels/videos?channelUrls=${encodeURIComponent(
       validUrls.join(",")
     )}`
   );
--- frontend/src/APIs/GetPaginatedVideos.ts
+++ frontend/src/APIs/GetPaginatedVideos.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 import type { PaginatedVideos } from "../Types/Types";
 
 const BaseURL = process.env.REACT_APP_API_URL;
+console.log(BaseURL);
 
 export const GetPaginatedVideos = async (params: URLSearchParams) => {
   const response = await axios.get<PaginatedVideos>(
-    `${BaseURL}/api/channels/paginated-videos?${params.toString()}`
+    `${BaseURL}api/channels/paginated-videos?${params.toString()}`
   );
 
   return response.data;
