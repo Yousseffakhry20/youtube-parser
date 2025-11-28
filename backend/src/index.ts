fc957026dc35a4b76c8617399f8b6d3ddc53cc56
Yousseffakhry20 <yousseffakhry96@gmail.com>
2025-11-28 08:41:22 +0200

    ***Backend: -Reverted open acces in CORS temporarily.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,10 +20,8 @@ mongoose
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    
   })
 );
 
