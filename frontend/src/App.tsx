b9ebd713dd63d4063254331ac48879ac3000a810
Yousseffakhry20 <yousseffakhry96@gmail.com>
2025-12-04 13:18:36 +0200

    ***Frontend: -Emergency React Version Update.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import Layout from './Components/Layout'
-import About from './Pages/About'
 import AllVideos from './Pages/AllVideos'
 
 function App() {
